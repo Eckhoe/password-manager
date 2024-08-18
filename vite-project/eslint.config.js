b38213d4b95c575b6d1be9d96a373a8f4fddf147
Eckhoee <8399460+Eckhoee@users.noreply.github.com>
2024-08-18 18:04:55 -0400

    Added typescript-eslint compatibility

--- vite-project/eslint.config.js
+++ vite-project/eslint.config.js
@@ -2,10 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
 ];
